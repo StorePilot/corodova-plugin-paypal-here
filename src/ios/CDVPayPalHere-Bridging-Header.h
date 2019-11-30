e441391f164f2366f130df58b83f366540d1d3ca
Steffan Halvorsen <steffanhalv@gmail.com>
2019-11-30 18:29:08 +0100

    Uncommented import as it is done in swift

--- src/ios/CDVPayPalHere-Bridging-Header.h
+++ src/ios/CDVPayPalHere-Bridging-Header.h
@@ -1,5 +1,5 @@
-#import <SimpleKeychain/SimpleKeychain.h>
-#import <PPRetailInstrumentInterface/PPRetailInstrumentInterface.h>
-#import <Trustkit/Trustkit.h>
-#import <AFNetworking/AFNetworking.h>
-#import <PayPalRetailSDK/PayPalRetailSDK.h>
+// #import <SimpleKeychain/SimpleKeychain.h>
+// #import <PPRetailInstrumentInterface/PPRetailInstrumentInterface.h>
+// #import <Trustkit/Trustkit.h>
+// #import <AFNetworking/AFNetworking.h>
+// #import <PayPalRetailSDK/PayPalRetailSDK.h>
