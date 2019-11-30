5192274e9d8955315e632fa605880a8c27ccb051
Steffan Halvorsen <steffanhalv@gmail.com>
2019-11-30 15:43:27 +0100

    Removed import of cordova exec

--- www/CDVPayPalHere.js
+++ www/CDVPayPalHere.js
@@ -1,11 +1,9 @@
-var exec = require('cordova/exec')
-
 const parseString = val => (val || '').toString()
 const parseNumber = val => (Number(val) || 0)
 
 exports.initializeMerchant = function (args, success, error) {
   args = args || {}
-  exec(success, error, 'CDVPayPalHere', 'initializeMerchantCDV', [
+  cordova.exec(success, error, 'CDVPayPalHere', 'initializeMerchantCDV', [
     parseString(args.accessToken),
     parseString(args.refreshUrl),
     parseString(args.environment),
@@ -15,19 +13,19 @@ exports.initializeMerchant = function (args, success, error) {
 
 exports.connectToReader = function (args, success, error) {
   args = args || {}
-  exec(success, error, 'CDVPayPalHere', 'connectToReaderCDV', [
+  cordova.exec(success, error, 'CDVPayPalHere', 'connectToReaderCDV', [
   ])
 }
 
 exports.searchAndConnectToReader = function (args, success, error) {
   args = args || {}
-  exec(success, error, 'CDVPayPalHere', 'searchAndConnectToReaderCDV', [
+  cordova.exec(success, error, 'CDVPayPalHere', 'searchAndConnectToReaderCDV', [
   ])
 }
 
 exports.takePayment = function (args, success, error) {
   args = args || {}
-  exec(success, error, 'CDVPayPalHere', 'takePaymentCDV', [
+  cordova.exec(success, error, 'CDVPayPalHere', 'takePaymentCDV', [
     parseString(args.currencyCode),
     parseNumber(args.total),
     parseString(args.invoiceNumber)
